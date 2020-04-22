04bbf9fa12354f8afdf37321cbc05c588fb62bb8
Leonid <leonidb93@gmail.com>
2020-04-22 02:47:59 +0200

    test player movement, define update and move calculation methods

--- src/ClientSarg.java
+++ src/ClientSarg.java
@@ -1,5 +1,6 @@
 import java.io.File;
 import java.io.IOException;
+import java.util.concurrent.ThreadLocalRandom;
 
 import javax.imageio.ImageIO;
 
@@ -7,7 +8,14 @@ import lenz.htw.sarg.Move;
 import lenz.htw.sarg.net.NetworkClient;
 
 public class ClientSarg implements Runnable {
+	private NetworkClient nc;
+
 	private String playerName;
+	private int playerNumber;
+
+	// Test
+	private int lastX;
+	private int lastY;
 
 	public ClientSarg(String name) throws IOException {
 		playerName = name;
@@ -32,7 +40,7 @@ public class ClientSarg implements Runnable {
 
 	@Override
 	public void run() {
-		NetworkClient nc;
+
 		try {
 			nc = new NetworkClient("127.0.0.1", playerName, ImageIO.read(new File("./bilder/phoenix.png")));
 
@@ -40,12 +48,59 @@ public class ClientSarg implements Runnable {
 
 			nc.getExpectedNetworkLatencyInMilliseconds();
 
-			nc.getMyPlayerNumber(); // 0 = rot, 1 = grün, 2 = blau
+			playerNumber = nc.getMyPlayerNumber(); // 0 = rot, 1 = grün, 2 = blau
+			System.out.println(playerNumber + " number");
 		} catch (IOException e) {
 			// TODO Auto-generated catch block
 			e.printStackTrace();
 		}
 
+		update();
+	}
+
+	private void update() {
+		while (true) {
+			Move receiveMove = nc.receiveMove();
+			if (receiveMove == null) {
+				// ich bin dran
+				Move move = calculateMove();
+				nc.sendMove(move);
+			} else {
+				// integriereZugInSpielbrett(move);
+			}
+		}
+	}
+
+	private Move calculateMove() {
+		Move move;
+		switch (playerNumber) {
+		case 0: {
+			int y = 0;
+
+			move = new Move(0, y + lastY);
+			lastY++;
+			break;
+		}
+		case 1: {
+			int x = 4;
+
+			move = new Move(x + lastX, 8);
+			lastX++;
+			break;
+		}
+		case 2: {
+			int x = 8;
+			int y = 4;
+
+			move = new Move(x + lastX, 4 + lastY);
+			lastX--;
+			lastY--;
+			break;
+		}
+		default:
+			throw new IllegalArgumentException("Unexpected value: " + playerNumber);
+		}
+		return move;
 	}
 
 }
