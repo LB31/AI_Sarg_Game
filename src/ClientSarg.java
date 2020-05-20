85fde73f34c37d754e57778e8652007cfabb6513
Leonid <leonidb93@gmail.com>
2020-05-21 00:22:35 +0200

    create playing field debugger, change streams to foreach loops, the previous bug got shifted but is still there

--- src/ClientSarg.java
+++ src/ClientSarg.java
@@ -3,6 +3,8 @@ import java.io.IOException;
 import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.List;
+import java.util.Optional;
+import java.util.Stack;
 import java.util.concurrent.ThreadLocalRandom;
 
 import javax.imageio.ImageIO;
@@ -20,18 +22,17 @@ public class ClientSarg implements Runnable {
 
 	private PlayingField mainField;
 	// For Alpha-Beta Pruning
-	private List<PlayingField> alphaBetaFields = new ArrayList<PlayingField>();
+//	private List<PlayingField> alphaBetaFields = new ArrayList<PlayingField>();
+	private Stack<PlayingField> alphaBetaFields = new Stack<>();
 
 	private Vector2D[][] moveDirections = new Vector2D[][] { { new Vector2D(0, 1), new Vector2D(1, 1) },
 			{ new Vector2D(1, 0), new Vector2D(0, -1) }, { new Vector2D(-1, -1), new Vector2D(-1, 0) } };
 
-	private List<Integer> otherPlayers = new ArrayList<>() {
-		{
+	private List<Integer> otherPlayers = new ArrayList<>() {{
 			add(0);
 			add(1);
 			add(2);
-		}
-	};
+		}};
 
 	private int searchDepth = 4;
 
@@ -118,14 +119,14 @@ public class ClientSarg implements Runnable {
 	private Move calculateMove() {
 		Move move = null;
 
-//		alphaBetaFields.add(newPlayingFieldCopy(mainField));
-//		AlphaBeta(searchDepth, Integer.MIN_VALUE, Integer.MAX_VALUE, 1);
-//		alphaBetaFields = new ArrayList<PlayingField>();
+		alphaBetaFields.push(newPlayingFieldCopy(mainField));
+		AlphaBeta(searchDepth, Integer.MIN_VALUE, Integer.MAX_VALUE, 1);
+		alphaBetaFields.empty();
 
-		Token[] myTokens = mainField.tokenPositions.stream().filter(t -> t.mine).toArray(Token[]::new);
-		int randomNum = ThreadLocalRandom.current().nextInt(0, myTokens.length);
-		Token token = myTokens[randomNum];
-//		Token token = bestTokenToMove;
+//		Token[] myTokens = mainField.tokenPositions.stream().filter(t -> t.mine).toArray(Token[]::new);
+//		int randomNum = ThreadLocalRandom.current().nextInt(0, myTokens.length);
+//		Token token = myTokens[randomNum];
+		Token token = bestTokenToMove;
 		move = new Move(token.x, token.y);
 
 		return move;
@@ -136,16 +137,19 @@ public class ClientSarg implements Runnable {
 
 		if (alphaBeta) {
 			// create a new playing field with the stats from the last move
-			playingField = newPlayingFieldCopy(alphaBetaFields.get(alphaBetaFields.size() - 1));
-
+//			playingField = newPlayingFieldCopy(alphaBetaFields.get(alphaBetaFields.size() - 1));
+			playingField = newPlayingFieldCopy(alphaBetaFields.peek());
 		} else { // for the actual updating of the original field
 			playingField = mainField;
 		}
 
-		Token tokenToMove = playingField.tokenPositions.stream()
-				.filter(t -> t.x == move.x && t.y == move.y)
-				.findAny()
-				.get();
+//		Token tokenToMove = playingField.tokenPositions.stream().filter(t -> t.x == move.x && t.y == move.y).findAny()
+//				.get();
+		Token tokenToMove = null;
+		for (Token token : playingField.tokenPositions) {
+			if(token.x == move.x && token.y == move.y) tokenToMove = token;
+		}
+		
 		for (int i = 0; i < moveDirections[0].length; i++) {
 
 			for (int j = 1; j <= playingField.playfield.length; j++) {
@@ -165,9 +169,17 @@ public class ClientSarg implements Runnable {
 					} else {
 						// jumped over a token
 						playingField.playfield[newX][newY] = 0;
-						Token killedToken = playingField.tokenPositions.stream().filter(t -> t.x == newX && t.y == newY)
-								.findFirst().get();
-						playingField.tokenPositions.remove(killedToken);
+//						boolean error = playingFieldDebugger(playingField);
+//
+//						Optional<Token> killedToken = playingField.tokenPositions.stream().filter(t -> t.x == newX && t.y == newY)
+//								.findFirst();
+//						boolean empty = true;
+//						if(killedToken.isEmpty()) empty = true;
+						Token finalToken = null;
+						for (Token token : playingField.tokenPositions) {
+							if(token.x == newX && token.y == newY) finalToken = token;
+						}
+						playingField.tokenPositions.remove(finalToken);
 					}
 				} catch (IndexOutOfBoundsException e) { // point outside of the playing field array
 					playingField.scores[tokenToMove.owner]++;
@@ -179,9 +191,10 @@ public class ClientSarg implements Runnable {
 		playingField.tokenPositions.remove(tokenToMove);
 		playingField.playfield[move.x][move.y] = 0;
 
-		if (alphaBeta) 
-			alphaBetaFields.add(playingField);
-
+		if (alphaBeta) {
+//			alphaBetaFields.add(playingField);		
+			alphaBetaFields.push(playingField);
+		}
 
 	}
 
@@ -190,22 +203,26 @@ public class ClientSarg implements Runnable {
 	 * @param player: 1 = max, 2 = min, 3 = min
 	 */
 	private int AlphaBeta(int depth, int alpha, int beta, int player) {
-		System.out.println(depth + " current depth");
-		System.out.println(alphaBetaFields.size() + " size list");
+//		System.out.println(depth + " current depth");
+//		System.out.println(alphaBetaFields.size() + " size list");
 		// TODO Züge sortieren, z.B. danach, wv Felder bis zum Punkt noch gegangen
 		// werden müssen
 		if (depth == 0 /* TODO game over in current position */) {
-			return calculateRating(alphaBetaFields.get(alphaBetaFields.size() - 1));
+//			return calculateRating(alphaBetaFields.get(alphaBetaFields.size() - 1));
+			return calculateRating(alphaBetaFields.peek());
 		}
 
+		// TODO Spielfeld 2D auf Referenzen umstellen
 		if (player == 1) {
 			int maxEval = alpha;
-			Token[] currentTokens = alphaBetaFields.get(searchDepth - depth).tokenPositions.stream().filter(t -> t.mine)
+			Token[] currentTokens = alphaBetaFields.peek().tokenPositions.stream().filter(t -> t.mine)
 					.toArray(Token[]::new);
+//					alphaBetaFields.get(searchDepth - depth).tokenPositions.stream().filter(t -> t.mine).toArray(Token[]::new);
 			for (Token token : currentTokens) {
 				updatePlayfield(new Move(token.x, token.y), true); // make move
 				int eval = AlphaBeta(depth - 1, maxEval, beta, 2);
-				alphaBetaFields.remove(searchDepth - depth); // undo move
+//				alphaBetaFields.remove(searchDepth - depth); // undo move
+				alphaBetaFields.pop();
 //				maxEval = Math.max(maxEval, eval);
 				if (eval > maxEval) {
 					maxEval = eval;
@@ -220,12 +237,13 @@ public class ClientSarg implements Runnable {
 
 		} else if (player == 2) {
 			int minEval = beta;
-			Token[] currentTokens = alphaBetaFields.get(searchDepth - depth).tokenPositions.stream()
+			Token[] currentTokens = alphaBetaFields.peek().tokenPositions.stream()
 					.filter(t -> t.owner == otherPlayers.get(0)).toArray(Token[]::new); // get is hard coded here
 			for (Token token : currentTokens) {
 				updatePlayfield(new Move(token.x, token.y), true); // make move
 				int eval = AlphaBeta(depth - 1, alpha, minEval, 3);
-				alphaBetaFields.remove(searchDepth - depth); // undo move
+//				alphaBetaFields.remove(searchDepth - depth); // undo move
+				alphaBetaFields.pop();
 				if (eval < minEval) {
 					minEval = eval;
 					if (minEval <= alpha)
@@ -235,12 +253,13 @@ public class ClientSarg implements Runnable {
 			return minEval;
 		} else {
 			int minEval = beta;
-			Token[] currentTokens = alphaBetaFields.get(searchDepth - depth).tokenPositions.stream()
+			Token[] currentTokens = alphaBetaFields.peek().tokenPositions.stream()
 					.filter(t -> t.owner == otherPlayers.get(1)).toArray(Token[]::new); // get is hard coded here
 			for (Token token : currentTokens) {
 				updatePlayfield(new Move(token.x, token.y), true); // make move
-				int eval = AlphaBeta(depth - 1, alpha, minEval, 1);				
-				alphaBetaFields.remove(searchDepth - depth); // undo move
+				int eval = AlphaBeta(depth - 1, alpha, minEval, 1);
+//				alphaBetaFields.remove(searchDepth - depth); // undo move
+				alphaBetaFields.pop();
 				if (eval < minEval) {
 					minEval = eval;
 					if (minEval <= alpha)
@@ -264,4 +283,22 @@ public class ClientSarg implements Runnable {
 
 	}
 
+	private boolean playingFieldDebugger(PlayingField currentField) {
+		boolean error = true;
+		for (int row = 0; row < currentField.playfield.length; row++) {
+            for (int col = 0; col < currentField.playfield[row].length; col++) {
+            	
+            	int curVal = currentField.playfield[row][col];
+            	int r = row; int c = col;
+            	if(curVal != 0 && curVal != -1) {
+            		for (Token token : currentField.tokenPositions) {
+						if(token.x == r && token.y == col) error = false;
+					}
+            	}
+            	
+            }
+        }
+		return error;
+	}
+	
 }
